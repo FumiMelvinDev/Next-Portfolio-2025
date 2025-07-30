7ae972726aaba83932a20703bebd0c4faca52dc6
FumiMelvinDev <fumimelvindev@gmail.com>
2025-07-30 15:48:54 +0200

    refactor: Remove navigation links from header component

--- components/header.tsx
+++ components/header.tsx
@@ -4,20 +4,7 @@ const Header = () => {
   return (
     <div className="flex justify-between items-center py-4">
       <div>Fumi Mel</div>
-      <div className="flex space-x-4 body-2 text-custom-700">
-        <a href="#about" className="hover:underline">
-          About
-        </a>
-        <a href="#skills" className="hover:underline">
-          Skills
-        </a>
-        <a href="#projects" className="hover:underline">
-          Projects
-        </a>
-        <a href="#contact" className="hover:underline">
-          Contact
-        </a>
-      </div>
+
       <ModeToggle />
     </div>
   );
