1e7a42f86a1eacad3c0e9dfd71e757f5fa5ef2b6
FumiMelvinDev <fumimelvindev@gmail.com>
2025-08-21 13:45:19 +0200

    fix: Add missing React import in page component

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import React from "react";
 
 import { formatDate } from "@/lib/utils";
 import MDXContent from "@/components/mdx-content";
